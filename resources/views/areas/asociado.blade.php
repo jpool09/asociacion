7a41b11019aea3f1ecb2eba6118a39502ddb1481
Jean Pool <jcruzcespedes72@yahoo.com>
2019-03-21 00:50:52 -0600

    primer commit

---
+++ resources/views/areas/asociado.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.layout')
+
+@section('contenido')
+
+    Asociados
+        
+@endsection
+
+@section('link')
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+@endsection
+
+@section('Tipo')
+<br><br><br>
+            <ul class="gap">
+            <li><a href="#?"><img class="logo" src="img/Asociación.png" alt="AsoScotiabank"></a></li>
+            <li><a href="home">Home</a></li>
+            <li><a href="#?">Información General</a></li>
+            <li><a href="#?">Información Crediticia</a></li>
+            <li><a href="#?">Subsidios</a></li>
+            <li><a href="#?">Pólizas</a></li>
+            <li><a href="#?">Garantías</a></li>
+            </ul>        
+@endsection
